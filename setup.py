42069ac5598dfa0bfe5e19dd532ec10faa3a39f5
adilm2245 <adilm2245@gmail.com>
2024-09-08 00:05:31 +0200

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Adil",
+    author_email="adilm2245@gmail.com",
+    packages=find_packages()
+)
